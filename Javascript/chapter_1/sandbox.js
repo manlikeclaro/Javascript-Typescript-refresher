a4822ec9803b76ee1eaac86831ac486d5510d3e6
manlikeclaro <ckibii08@gmail.com>
2024-07-12 17:35:27 +0300

    [feat] implemented arrow functions

--- Javascript/chapter_1/sandbox.js
+++ Javascript/chapter_1/sandbox.js
@@ -1,16 +1,47 @@
 let isClicked = false;
+const firstElement = document.querySelector('#firstElement').innerHTML;
+const secondElement = document.querySelector('#secondElement').innerHTML;
+const thirdElement = document.querySelector('#thirdElement').innerHTML;
+const ninjas = ["chun li", "jet li", "jackie chan", "bruce lee"];
+
 console.log(`I am inactive. the value of isClicked is: ${isClicked}`);
 
 document.querySelector('#button').onclick = function () {
-    // alert("Button is clicked");
     isClicked = !isClicked;
     console.log(`I am activated. the value of isClicked is: ${isClicked}`);
     buttonClicked();
 }
 
-const initialHtml = document.querySelector('.lead').innerHTML;
+document.querySelector('#button2').onclick = function () {
+    isClicked = !isClicked;
+    console.log(`I am activated. the value of isClicked is: ${isClicked}`);
+    printNinjas();
+}
+
+document.querySelector('#button3').onclick = function () {
+    document.querySelector('#thirdElement').innerHTML = getName("Maria");
+}
 
 function buttonClicked(event) {
-    document.querySelector('.lead').innerHTML =
-        isClicked ? 'Inner HTML updated when clicked!' : `Reverted to - ${initialHtml}`;
+    document.querySelector('#firstElement').innerHTML =
+        isClicked ? 'Inner HTML updated when clicked!' : `Reverted to - ${firstElement}`;
+}
+
+function logNinjas() {
+    let loggedNinjas = '';
+    for (let ninja of ninjas) {
+        loggedNinjas += `<div><p>${ninja}</p></div>`;
+    }
+    // console.log(loggedNinjas);
+    return loggedNinjas;
+}
+
+function printNinjas() {
+    // logNinjas();
+    document.querySelector('#secondElement').innerHTML =
+        isClicked ? logNinjas() : `Reverted to ${secondElement}`;
+}
+
+const getName = (string) => {
+    return `${string} is your name`;
 }
