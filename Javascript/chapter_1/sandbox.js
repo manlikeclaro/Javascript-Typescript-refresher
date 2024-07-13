822f0587ae8c2305842bcfc03658faaeb4057b0c
manlikeclaro <ckibii08@gmail.com>
2024-07-13 23:25:12 +0300

    [chore] restructured html

--- Javascript/chapter_1/sandbox.js
+++ Javascript/chapter_1/sandbox.js
@@ -4,44 +4,41 @@ const secondElement = document.querySelector('#secondElement').innerHTML;
 const thirdElement = document.querySelector('#thirdElement').innerHTML;
 const ninjas = ["chun li", "jet li", "jackie chan", "bruce lee"];
 
-console.log(`I am inactive. the value of isClicked is: ${isClicked}`);
+console.log(`I am inactive. The value of isClicked is: ${isClicked}`);
 
-document.querySelector('#button').onclick = function () {
+document.querySelector('#button').onclick = () => {
     isClicked = !isClicked;
-    console.log(`I am activated. the value of isClicked is: ${isClicked}`);
+    console.log(`I am activated. The value of isClicked is: ${isClicked}`);
     buttonClicked();
-}
+};
 
-document.querySelector('#button2').onclick = function () {
+document.querySelector('#button2').onclick = () => {
     isClicked = !isClicked;
-    console.log(`I am activated. the value of isClicked is: ${isClicked}`);
+    console.log(`I am activated. The value of isClicked is: ${isClicked}`);
     printNinjas();
-}
+};
 
-document.querySelector('#button3').onclick = function () {
-    document.querySelector('#thirdElement').innerHTML = getName("Maria");
-}
+document.querySelector('#button3').onclick = () => {
+    isClicked = !isClicked;
+    document.querySelector('#thirdElement').innerHTML =
+        isClicked ? getName("Maria") : thirdElement;
+};
 
-function buttonClicked(event) {
-    document.querySelector('#firstElement').innerHTML =
-        isClicked ? 'Inner HTML updated when clicked!' : `Reverted to - ${firstElement}`;
+function buttonClicked() {
+    document.querySelector('#firstElement').innerHTML = isClicked ? 'Inner HTML updated when clicked!' : `Reverted to - ${firstElement}`;
 }
 
 function logNinjas() {
     let loggedNinjas = '';
-    for (let ninja of ninjas) {
-        loggedNinjas += `<div><p>${ninja}</p></div>`;
-    }
-    // console.log(loggedNinjas);
+    ninjas.forEach(item => {
+        loggedNinjas += `<div><p>${item}</p></div>`;
+    });
+    console.log(loggedNinjas);
     return loggedNinjas;
 }
 
 function printNinjas() {
-    // logNinjas();
-    document.querySelector('#secondElement').innerHTML =
-        isClicked ? logNinjas() : `Reverted to ${secondElement}`;
+    document.querySelector('#secondElement').innerHTML = isClicked ? logNinjas() : `Reverted to ${secondElement}`;
 }
 
-const getName = (string) => {
-    return `${string} is your name`;
-}
+const getName = (string) => `${string} is your name`;
