0143afe26245de9ded8f0d0420e4c780bf32b7d3
manlikeclaro <ckibii08@gmail.com>
2024-07-15 12:30:00 +0300

    [learn] performed dom manipulation

--- Javascript/chapter_1/sandbox.js
+++ Javascript/chapter_1/sandbox.js
@@ -1,44 +1,70 @@
 let isClicked = false;
-const firstElement = document.querySelector('#firstElement').innerHTML;
+const firstElement = document.querySelector('#firstElement').innerText;
 const secondElement = document.querySelector('#secondElement').innerHTML;
 const thirdElement = document.querySelector('#thirdElement').innerHTML;
+const fourthElement = document.querySelectorAll('#fourthElement > li');
+
+// console.log(fourthElement.innerHTML);
+
+const changeClass = () => {
+    fourthElement.forEach(item => {
+        if (item.textContent.includes('Lorem')) {
+            console.log(item);
+            item.classList.add('active');
+        } else {
+            item.classList.add('text-danger');
+        }
+    });
+}
+
+changeClass(); // Call the function to apply the changes
+
 const ninjas = ["chun li", "jet li", "jackie chan", "bruce lee"];
 
-console.log(`I am inactive. The value of isClicked is: ${isClicked}`);
+let user = {
+    username: "John Smith",
+    age: 25,
+    details() {
+        console.log(`user details logged`);
+        return `${this.username} is ${this.age} yrs old`;
+    }
+}
 
 document.querySelector('#button').onclick = () => {
-    isClicked = !isClicked;
     console.log(`I am activated. The value of isClicked is: ${isClicked}`);
-    buttonClicked();
+    buttonClicked("HTML updated", firstElement, 'firstElement');
 };
 
 document.querySelector('#button2').onclick = () => {
-    isClicked = !isClicked;
     console.log(`I am activated. The value of isClicked is: ${isClicked}`);
-    printNinjas();
+    buttonClicked(logNinjas(), secondElement, 'secondElement');
 };
 
 document.querySelector('#button3').onclick = () => {
-    isClicked = !isClicked;
-    document.querySelector('#thirdElement').innerHTML =
-        isClicked ? getName("Maria") : thirdElement;
+    console.log(`I am activated. The value of isClicked is: ${isClicked}`);
+    buttonClicked(getName('Maria the angel'), thirdElement, 'thirdElement');
 };
 
-buttonClicked = () => {
-    document.querySelector('#firstElement').innerHTML = isClicked ? 'Inner HTML updated when clicked!' : `Reverted to - ${firstElement}`;
+document.querySelector('#button4').onclick = () => {
+    console.log(user.details());
+}
+
+buttonClicked = (firstParam, secondParam, thirdParam) => {
+    isClicked = !isClicked;
+    const element = document.querySelector(`#${thirdParam}`)
+    element.innerHTML = isClicked ? firstParam : secondParam;
+    if (thirdParam === 'firstElement') {
+        isClicked ?
+            element.classList.add('text-danger') : element.classList.remove('text-danger');
+    }
 }
 
 logNinjas = () => {
-    let loggedNinjas = '';
-    ninjas.forEach(item => {
-        loggedNinjas += `<div><p>${item}</p></div>`;
+    let loggedNinjas = secondElement;
+    ninjas.forEach((item, index) => {
+        loggedNinjas += `<li class="list-group-item">${index} - ${item}</li>`;
     });
-    console.log(loggedNinjas);
     return loggedNinjas;
-}
-
-printNinjas = () => {
-    document.querySelector('#secondElement').innerHTML = isClicked ? logNinjas() : `Reverted to ${secondElement}`;
 }
 
 const getName = (string) => `${string} is your name`;
